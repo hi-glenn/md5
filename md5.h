e78dcb909a7df6e2280d1dcf123fdc5b4f77b5d7
hi-glenn <promise_wg@hotmail.com>
2025-11-29 15:40:55 +0800

    format

--- md5.h
+++ md5.h
@@ -6,11 +6,10 @@ typedef struct
 	unsigned int count[2];
 	unsigned int state[4];
 	unsigned char buffer[64];
-}MD5_CTX;
+} MD5_CTX;
 
 void MD5Init(MD5_CTX *context);
-void MD5Update(MD5_CTX *context,unsigned char *input,unsigned int inputlen);
-void MD5Final(MD5_CTX *context,unsigned char digest[16]);
+void MD5Update(MD5_CTX *context, unsigned char *input, unsigned int inputlen);
+void MD5Final(MD5_CTX *context, unsigned char digest[16]);
 
 #endif
-
